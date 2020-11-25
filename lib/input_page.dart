a561bef2b78e1ff67375c944880e13d5852bda06
mohsinraza079 <mraza6797@gmail.com>
2020-11-25 10:33:54 +0500

    Done Lecture 59 : Code Sepration in Different Files

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_text_file.dart';
+import 'container_file.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -64,50 +66,6 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class RepeatTextandIconeWidget extends StatelessWidget {
-  RepeatTextandIconeWidget({@required this.iconData,this.label});
- final IconData iconData;
- final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          iconData,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          label,
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Color(0xFF8D8E98),
-          ),
-        )
-      ],
 
-    );
-  }
-}
 
-class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors,this.cardWidget});
-  final Color colors;
-  final Widget cardWidget;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      child: cardWidget,
-
-      decoration: BoxDecoration(
-        color: colors,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
