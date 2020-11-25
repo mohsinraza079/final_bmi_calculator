843975179492b6217db4c362b17b178ff34d59c8
mohsinraza079 <mraza6797@gmail.com>
2020-11-25 10:21:37 +0500

    Done Lecture 57 II : Use of ReFactored  Code

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -18,10 +20,23 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: RepeatContainerCode(colors: Color(0xFF1D1E33),)
+                    child: RepeatContainerCode(
+                      colors: Color(0xFF1D1E33),
+                      cardWidget: RepeatTextandIconeWidget(
+                        iconData: FontAwesomeIcons.male,
+                        label: 'MALE',
+                      ),
+                    )
                   ),
                   Expanded(
-                    child: RepeatContainerCode(colors: Color(0xFF1D1E33),)
+                    child: RepeatContainerCode(
+                      colors: Color(0xFF1D1E33),
+                      cardWidget: RepeatTextandIconeWidget(
+                        iconData: FontAwesomeIcons.female,
+                        label: 'FEMALE',
+                      ),
+
+                    )
                   ),
                 ],
               )
@@ -49,14 +64,46 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class RepeatTextandIconeWidget extends StatelessWidget {
+  RepeatTextandIconeWidget({@required this.iconData,this.label});
+ final IconData iconData;
+ final String label;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+
+    );
+  }
+}
+
 class RepeatContainerCode extends StatelessWidget {
-  RepeatContainerCode({@required this.colors});
+  RepeatContainerCode({@required this.colors,this.cardWidget});
   final Color colors;
+  final Widget cardWidget;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
+      child: cardWidget,
+
       decoration: BoxDecoration(
         color: colors,
         borderRadius: BorderRadius.circular(10.0),
