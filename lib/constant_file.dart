f93a6a1925bf7a5fe16932d1380c67767d2a57ba
mohsinraza079 <mraza6797@gmail.com>
2020-11-25 17:12:04 +0500

    Done Lecture 69 : Complete UI of 2nd Screen

--- lib/constant_file.dart
+++ lib/constant_file.dart
@@ -13,3 +13,25 @@ const kLargeButtonStyle = TextStyle(
   fontSize: 25.0,
   fontWeight: FontWeight.bold,
 );
+
+
+const kTitleStyleS2 = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultText = TextStyle(
+  fontSize: 20.0,
+  fontWeight: FontWeight.bold,
+  color: Colors.green,
+);
+
+const kBMiTextStyle = TextStyle(
+  fontSize: 60.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle= TextStyle(
+  fontSize: 15.0,
+  fontWeight: FontWeight.bold,
+);
--- lib/result_file.dart
+++ lib/result_file.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'constant_file.dart';
+import 'container_file.dart';
+import 'input_page.dart';
 
 class ResultScreen extends StatelessWidget {
   @override
@@ -7,7 +10,58 @@ class ResultScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("BMI Result"),
       ),
-      body: Text("Body Data"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+             child: Container(
+               child: Text('Your Result',style: kTitleStyleS2,),
+             ),
+          ),
+          Expanded(
+            flex: 5,
+            child: RepeatContainerCode(
+              colors: activeColor,
+              cardWidget: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Normal',
+                    style: kResultText,
+                  ),
+                  Text(
+                    '18.3',
+                    style: kBMiTextStyle,
+                  ),
+                  Text(
+                    'BMI is low you should have to work more',
+                    style: kBodyTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child:  GestureDetector(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>InputPage()));
+              } ,
+              child: Container(
+
+                child: Center(
+                    child: Text('ReCalculate',style: kLargeButtonStyle,)),
+                color: Color(0xFFEB1555),
+                margin: EdgeInsets.only(top: 10.0),
+                width: double.infinity,
+                height: 20.0,
+              ),
+            ),
+          ),
+        ],
+
+    )
     );
   }
 }
